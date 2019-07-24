90fbe779dd6a57de0273fb67fd9aab027acc5014
tori0359 <hij0359@gmail.com>
2019-07-24 17:11:38 +0900

    Spring day01

---
+++ sts01/src/com/bit/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.bit.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("IndexController...");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		return mav;
+	}
+
+	
+}
